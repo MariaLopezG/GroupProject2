cd86a67b2790a60bd3ed4814af3955de3d9096bf
adeamulaku <adeamulaku02@gmail.com>
2024-05-17 23:13:33 -0700

    updated paths

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,37 +1,37 @@
-const loginForm = document.getElementById('loginForm');
-const signupForm = document.getElementById('signupForm');
+// const loginForm = document.getElementById('loginForm');
+// const signupForm = document.getElementById('signupForm');
 
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
+// loginForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
 
-    try {
-        const formData = new FormData(loginForm);
-        const response = await fetch('/login', {
-            method: 'POST',
-            body: formData
-        });
+//     try {
+//         const formData = new FormData(loginForm);
+//         const response = await fetch('/login', {
+//             method: 'POST',
+//             body: formData
+//         });
 
-        const data = await response.json();
-        console.log(data); 
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
-});
+//         const data = await response.json();
+//         console.log(data); 
+//     } catch (error) {
+//         console.error('There was a problem with the fetch operation:', error);
+//     }
+// });
 
-signupForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
+// signupForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
 
-    try {
-        const formData = new FormData(signupForm);
-        const response = await fetch('/signup', {
-            method: 'POST',
-            body: formData
-        });
+//     try {
+//         const formData = new FormData(signupForm);
+//         const response = await fetch('/signup', {
+//             method: 'POST',
+//             body: formData
+//         });
 
-        const data = await response.json();
-        console.log(data); 
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
-});
+//         const data = await response.json();
+//         console.log(data); 
+//     } catch (error) {
+//         console.error('There was a problem with the fetch operation:', error);
+//     }
+// });
 
--- server/server.js
+++ server/server.js
@@ -10,22 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'views')));
 
-//Route for the root URL
+// Route for the root URL
 app.get('/', (req, res) => {
     // Send the index.html file as the response
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-//Route for other HTML pages
+// Route for other HTML pages
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
 });
 
-app.get('/login', (req, res) => {
+app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'signup.html'));
 });
 
-app.get('/login', (req, res) => {
+app.get('/cart', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'cart.html'));
 });
 
@@ -33,7 +33,7 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    // check if the username and password are valid
+    // Check if the username and password are valid
     db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
         if (err) {
             return res.status(500).json({ error: 'Internal Server Error' });
@@ -63,9 +63,7 @@ app.post('/signup', (req, res) => {
     });
 });
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
